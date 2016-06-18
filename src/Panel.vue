c27cb0273ad6afb9a41cb2a7ad54aaed89596771
sdlong <sdlong.jeng@gmail.com.tw>
2016-06-18 16:00:34 +0800

    Implement function: create new table

--- src/Panel.vue
+++ src/Panel.vue
@@ -90,6 +90,9 @@ export default {
       return;
     },
     create (){
+      if( typeof this.rows === 'undefined'){
+         this.$set('rows', []);
+      }
       this.newRows.id = new Date().getTime().toString();
       this.rows.push( this.newRows );
       this.reset();
